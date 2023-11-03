87e62b68539627f661d2d126370e02131ddb7a6a
developer <1735721264@qq.com>
2023-11-03 20:00:57 +0800

    x

--- src/View.Navigation.jsx
+++ src/View.Navigation.jsx
@@ -15,8 +15,8 @@ import Imitation from './utils.imitation'
 
 function Action() {
   return <>
-    {/* <Button style={{ margin: '4px 0', justifyContent: 'start' }} fullWidth variant='outlined' onClick={() =>  Imitation.assignState({ dialogAudioMultipleSetting: true })}><SettingsIcon style={{ marginRight: 4 }} />Audio Setting</Button> */}
-    <Button style={{ margin: '4px 0', justifyContent: 'start' }} fullWidth variant='outlined' onClick={() => Imitation.assignState({ dialogGlobalSetting: true })}><SettingsIcon style={{ marginRight: 4 }} />Global Setting</Button>
+    {/* <Button style={{ marginTop: 4, justifyContent: 'start' }} fullWidth variant='outlined' onClick={() =>  Imitation.assignState({ dialogAudioMultipleSetting: true })}><SettingsIcon style={{ marginRight: 4 }} />Audio Setting</Button> */}
+    <Button style={{ marginTop: 4, justifyContent: 'start' }} fullWidth variant='outlined' onClick={() => Imitation.assignState({ dialogGlobalSetting: true })}><SettingsIcon style={{ marginRight: 4 }} />Global Setting</Button>
   </>
 }
 
@@ -38,8 +38,8 @@ function App() {
     <Drawer anchor='left' open={Imitation.state.navigationDrawer} onClose={() => Imitation.assignState({ navigationDrawer: false })} sx={{ '& .MuiPaper-root': { width: 304, height: '100%' } }} >
       <div style={{ height: '100%', padding: 16, display: 'flex', flexDirection: 'column', justifyContent: 'space-between', overflow: 'auto' }}>
         <div>
-          <Button style={{ margin: '4px 0', justifyContent: 'start' }} fullWidth variant={pathname === '/SimplePiano' ? 'contained' : 'outlined'} onClick={() => { push('/SimplePiano'); Imitation.assignState({ navigationDrawer: false }); }}><MusicNoteIcon style={{ marginRight: 4 }} />Simple Piano</Button>
-          <Button style={{ margin: '4px 0', justifyContent: 'start' }} fullWidth variant={pathname === '/BassoonStacF1' ? 'contained' : 'outlined'} onClick={() => { push('/BassoonStacF1'); Imitation.assignState({ navigationDrawer: false }); }}><MusicNoteIcon style={{ marginRight: 4 }} />Bassoon StacF1</Button>
+          <Button style={{ marginBottom: 4, justifyContent: 'start' }} fullWidth variant={pathname === '/SimplePiano' ? 'contained' : 'outlined'} onClick={() => { push('/SimplePiano'); Imitation.assignState({ navigationDrawer: false }); }}><MusicNoteIcon style={{ marginRight: 4 }} />Simple Piano</Button>
+          <Button style={{ marginBottom: 4, justifyContent: 'start' }} fullWidth variant={pathname === '/BassoonStacF1' ? 'contained' : 'outlined'} onClick={() => { push('/BassoonStacF1'); Imitation.assignState({ navigationDrawer: false }); }}><MusicNoteIcon style={{ marginRight: 4 }} />Bassoon StacF1</Button>
         </div>
 
         <div>
--- src/View.Page.Console.jsx
+++ src/View.Page.Console.jsx
@@ -52,7 +52,7 @@ function Console() {
 
   const drag = { onDragOver, onDrop }
 
-  const maxTime = Imitation.state.consoleCurrent ? Math.max(...Imitation.state.consoleCurrent.group.map((i) => i.when + i.duration)) : 0
+  const maxTime = Imitation.state.consoleCurrent && Imitation.state.consoleCurrent.group.length > 0 ? Math.max(...Imitation.state.consoleCurrent.group.map((i) => i.when + i.duration / i.rate)) : 0
 
   React.useEffect(() => {
 
@@ -61,67 +61,82 @@ function Console() {
   return <div style={{ width: '100%', height: '100%', display: 'flex' }}>
 
     <div style={{ height: '100%', flexGrow: 0, flexShrink: 0, display: 'flex', flexDirection: 'column', overflow: 'auto' }}>
-      <Button style={{ margin: '4px 0' }} fullWidth variant='contained'><PlayArrowIcon /></Button>
-      <Button style={{ margin: '4px 0' }} fullWidth variant='contained'><PauseIcon /></Button>
-      <Button style={{ margin: '4px 0' }} fullWidth variant='contained' onClick={() => Imitation.setState(pre => { pre.console.push({ id: hash(6), name: hash(6), group: [] }); pre.consoleCurrent = pre.console[0]; return pre })}><PlaylistAddIcon /></Button>
+      <Button fullWidth variant='contained'><PlayArrowIcon /></Button>
+      <Button style={{ marginTop: 4 }} fullWidth variant='contained'><PauseIcon /></Button>
+      <Button style={{ marginTop: 4 }} fullWidth variant='contained' onClick={() => Imitation.setState(pre => { pre.console.push({ id: hash(6), name: hash(6), group: [] }); pre.consoleCurrent = pre.console[0]; return pre })}><PlaylistAddIcon /></Button>
       {
         Imitation.state.consoleCurrent ?
-          <Button style={{ margin: '4px 0' }} fullWidth variant='contained' onClick={() => Imitation.setState(pre => { pre.console = pre.console.filter(i => i !== pre.consoleCurrent); pre.consoleCurrent = null; return pre })}><DeleteIcon /></Button>
+          <Button style={{ marginTop: 4 }} fullWidth variant='contained' onClick={() => Imitation.setState(pre => { pre.console = pre.console.filter(i => i !== pre.consoleCurrent); pre.consoleCurrent = null; return pre })}><DeleteIcon /></Button>
           : null
       }
     </div>
 
-    <Divider style={{ margin: '0 8px' }} orientation='vertical' />
+    <Divider style={{ margin: '0 8px', borderColor: '#ffffff' }} orientation='vertical' />
 
     <div style={{ height: '100%', flexGrow: 0, flexShrink: 0, display: 'flex', flexDirection: 'column', overflow: 'auto' }}>
       {
         Imitation.state.console.map((i, index) => {
-          return <Animation tag={Button} restore={true} animation={[{ opacity: 0 }, { opacity: 1 }]} key={i.id} style={{ margin: '4px 0', textAlign: 'left', transition: '0.5s all' }} fullWidth variant={i === Imitation.state.consoleCurrent ? 'contained' : 'outlined'} onClick={() => Imitation.setState(pre => { pre.consoleCurrent = i; return pre })}>{i.name}</Animation>
+          return <Animation tag={Button} restore={true} animation={[{ opacity: 0 }, { opacity: 1 }]} key={i.id} style={{ marginTop: index !== 0 ? 4 : 0, textAlign: 'left', transition: '0.5s all' }} fullWidth variant={i === Imitation.state.consoleCurrent ? 'contained' : 'outlined'} onClick={() => Imitation.setState(pre => { pre.consoleCurrent = i; return pre })}>{i.name}</Animation>
         })
       }
     </div>
 
     {
-      Imitation.state.console.length > 0 ? <Divider style={{ margin: '0 8px' }} orientation='vertical' /> : null
+      Imitation.state.console.length > 0 ? <Divider style={{ margin: '0 8px', borderColor: '#ffffff' }} orientation='vertical' /> : null
     }
 
-    <div style={{ width: 0, height: '100%', flexGrow: 1, flexShrink: 0, overflow: 'auto', position: 'relative' }} {...drag}>
+    <div style={{ width: 0, height: '100%', flexGrow: 1, flexShrink: 0, padding: '0px 8px' }} {...drag}>
+      <div style={{ width: '100%', height: '100%', position: 'relative' }}>
 
-      <div style={{ width: '100%', height: 2, position: 'absolute', zIndex: 2, bottom: Imitation.state.consoleCurrent && Imitation.state.consoleCurrent.group.length > 0 ? 8 : 'calc(50% - 1px)', transition: '0.5s all' }}>
-        <div style={{ width: '100%', height: '100%', position: 'absolute', top: 0, bottom: 0, margin: 'auto', background: Imitation.state.theme.palette.primary.main }}></div>
+        <div style={{ width: '100%', height: 2, position: 'absolute', zIndex: 2, bottom: Imitation.state.consoleCurrent && Imitation.state.consoleCurrent.group.length > 0 ? 12 : 'calc(50% - 1px)', transition: '0.5s all' }}>
+          <div style={{ width: '100%', height: '100%', position: 'absolute', top: 0, bottom: 0, margin: 'auto', background: Imitation.state.theme.palette.primary.main }}></div>
 
-        <div style={{ width: '100%', height: '100%', position: 'absolute', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-          {
-            new Array(101).fill().map((i, index) => {
-              if (index % 10 === 0) {
-                return <div key={index} style={{ width: 2, height: 8, background: Imitation.state.theme.palette.primary.main, transition: '0.5s all' }}></div>
-              }
-              if (index % 5 === 0) {
-                return <div key={index} style={{ width: 2, height: 6, background: Imitation.state.theme.palette.primary.main, transition: '0.5s all' }}></div>
-              }
-              if (index % 10 !== 0 && index % 5 !== 0) {
-                return <div key={index} style={{ width: 2, height: 4, background: Imitation.state.theme.palette.primary.main, transition: '0.5s all' }}></div>
-              }
-            })
-          }
-        </div>
-      </div>
-
-      {
-        Imitation.state.consoleCurrent ?
-          <div style={{ width: '100%', height: '100%', position: 'absolute', overflow: 'auto' }}>
+          <div style={{ width: '100%', height: '100%', position: 'absolute', display: 'flex', alignItems: 'center' }}>
             {
-              Imitation.state.consoleCurrent.group.map((i, index) => {
-                return <ConsoleSource key={index} source={i} style={{ width: `${i.duration / maxTime * 100}%`, height: 40, top: index * 48, left: `${i.when / maxTime * 100}%` }} />
+              new Array(101).fill().map((i, index) => {
+                if (index % 10 === 0) {
+                  return <div key={index} style={{ position: 'absolute', left: `calc(${index}% - ${index * 2 / 100}px)`, width: 2, height: 8, background: Imitation.state.theme.palette.primary.main, transition: '0.5s all' }}></div>
+                }
+                if (index % 5 === 0) {
+                  return <div key={index} style={{ position: 'absolute', left: `calc(${index}% - ${index * 2 / 100}px)`, width: 2, height: 6, background: Imitation.state.theme.palette.primary.main, transition: '0.5s all' }}></div>
+                }
+                if (index % 10 !== 0 && index % 5 !== 0) {
+                  return <div key={index} style={{ position: 'absolute', left: `calc(${index}% - ${index * 2 / 100}px)`, width: 2, height: 4, background: Imitation.state.theme.palette.primary.main, transition: '0.5s all' }}></div>
+                }
               })
             }
           </div>
-          : null
-      }
 
+          <div style={{ width: '100%', height: '100%', position: 'absolute', display: 'flex', alignItems: 'center' }}>
+            {
+              new Array(11).fill().map((i, index) => {
+                return <div key={index} style={{ position: 'absolute', left: `${index * 10}%`, top: 8, width: 0, display: 'flex', justifyContent: 'center', color: Imitation.state.theme.palette.primary.main, transition: '0.5s all' }}>
+                  <div style={{ whiteSpace: 'nowrap' }}>
+                    {maxTime ? Number(index * maxTime / 10).toFixed(2) : 0}
+                  </div>
+                </div>
+              })
+            }
+          </div>
+        </div>
+
+
+        {
+          Imitation.state.consoleCurrent ?
+            <div style={{ width: '100%', height: '100%', position: 'absolute', overflow: 'auto' }}>
+              {
+                Imitation.state.consoleCurrent.group.map((i, index) => {
+                  return <ConsoleSource key={index} source={i} style={{ width: `${i.duration / i.rate / maxTime * 100}%`, height: 40, top: index * 48, left: `${i.when / maxTime * 100}%` }} />
+                })
+              }
+            </div>
+            : null
+        }
+
+      </div>
     </div>
 
-  </div>
+  </div >
 }
 
 function App() {
